b9a4e9f988bcfe1e3d4b124ad0205027f914c973
Diego Prats <diego.prats@gmail.com>
2015-02-01 12:57:07 -0800

    refactor performOperation() for readability

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,16 +10,97 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Display Value
+    @IBOutlet weak var display: UILabel!
+    
+    //operand stack
+    var operandStack:[Double] = []
+    //keeping track of whether we are in middle of a digit or finished
+    var inMiddleOfTyping: Bool = false
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            inMiddleOfTyping = false
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    //func for DIGITS
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        //if first digit pressed
+        if !inMiddleOfTyping  {
+            display.text = digit
+            inMiddleOfTyping = true
+            
+        }
+        //if second digit pressed
+        else {
+            //add the digit to the current number
+            display.text = display.text! + digit
+            
+        }
     }
-
-
+    
+    
+//
+//    @IBAction func enter(sender: UIButton) {
+//        
+//      
+//        
+//    }
+    
+    //func for ENTER
+    @IBAction func enter() {
+        inMiddleOfTyping = false
+        
+        //add the display to the operandStack
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    
+    //func for an OPERATION
+    @IBAction func operate(sender: UIButton) {
+        enter()
+        let operation = sender.currentTitle!
+        println("operandStack = \(operandStack)")
+        println("operation = \(operation)")
+        
+        
+        //perform the last operation pressed on the last two digits
+        // 5 = performOperation("add")
+        switch operation{
+            case "+": performOperation(add)
+//            case "−": performOperation("subtract")
+//            case "×": performOperation("multiply")
+//            case "÷": performOperation("divide")
+            
+            default:
+                break
+        }
+    }
+    
+    func performOperation(operation: (Double,Double) -> Double) {
+            if operandStack.count >= 2 {
+                displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+                
+                //add the result to the stack
+                enter()
+            }
+    }
+    
+    func add(op1: Double, op2: Double) -> Double {
+        return op1 + op2
+    }
+    
 }
+
+//TEST2
 
